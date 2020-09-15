54ba685fdee9ab5d7a4ef7695a877838a3b629d0
Dmitriy <iqaengineercom1group@gmail.com>
2020-09-15 16:04:14 +0300

    add button check

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMaried: false
+};
+
+console.log(obj.age);
+
+let arr = [
+    'plump.jpg',
+    'orage.jpg'
+];
+
+console.log(arr[0]);
+
+// alert('hello');
+
+//const result = confirm('are you here?');
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+
+const answers = [];
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
+
+    const isChekcked = true,
+            isClose = false;
+        console.log(isChekcked || isClose);
